f5c7cdcb6e4a198207e6e42961271cff45229b2f
Mostafa3397 <abdelaalmostafa5@gmail.com>
2025-04-25 12:31:44 +0300

    Add files via upload

---
+++ int/config.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+    define('PATH', realpath('.'));
+    define('SUBFOLDER', false);
+    define('URL', 'https://smmfollows.smmpanelscript.store');
+       define('ADMIN_URL', 'admin');//dont change if dont know about that
+
+  
+    ini_set('display_errors', 0);
+    date_default_timezone_set('Asia/Kolkata');
+    
+    return [
+      'db' => [
+        'name'    =>  'u307874657_smmfollows',
+        'host'    =>  'localhost',
+        'user'    =>  'u307874657_smmfollows',
+        'pass'    =>  'Rt;3^K#rxoR',
+        'charset' =>  'utf8mb4' 
+      ]
+    ];
+    
